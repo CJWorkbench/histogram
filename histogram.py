fc724c12e6eb797998d9ab5d06b579e3329a0bea
Adam Hooper <adam@adamhooper.com>
2018-07-18 13:24:57 -0400

    More styling

--- histogram.py
+++ histogram.py
@@ -58,6 +58,7 @@ def render(table, params):
                 'type': 'quantitative',
                 'axis': {
                     'title': 'Count',
+                    'domain': False,
                 },
             },
             'y': {
