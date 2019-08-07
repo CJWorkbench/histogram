784b9c3218f497b895e2720d34ba0e0c82fed6ba
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:34:41 -0400

    Test with Pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='histogram',
-    version='0.0.1',
-    description='Present a columnful of numbers as a histogram',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/histogram',
-    packages=[''],
-    py_modules=['histogram'],
-    install_requires=['pandas==0.24.1']
+    name="histogram",
+    version="0.0.1",
+    description="Present a columnful of numbers as a histogram",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/histogram",
+    packages=[""],
+    py_modules=["histogram"],
+    install_requires=["pandas==0.25.0"],
 )
